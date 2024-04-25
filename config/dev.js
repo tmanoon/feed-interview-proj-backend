cd384b4857f0db7e47bdfe0095b6fe70f889292c
tmanoon <151874385+tmanoon@users.noreply.github.com>
2024-04-25 15:39:44 +0300

    Add necessary files of backend

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+export default  {
+  dbURL: 'mongodb+srv://sabshoval:manoon@cluster0.mw35xzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  // dbURL: 'mongodb://127.0.0.1:27017',
+  dbName : 'comment_db'
+}
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+export default {
+  dbURL: 'mongodb+srv://sabshoval:manoon@cluster0.mw35xzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbName : process.env.DB_NAME || 'comment_db'
+}
